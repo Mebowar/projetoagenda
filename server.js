1bbad33803c838041ab0f4fabe5b665de81aeb0d
Mebowzinho <jorghbl@hotmail.com>
2023-06-12 18:38:57 -0300

    att

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ mongoose.connect(process.env.CONNECTIONSTRING,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useFindAndModify: false
+    // useFindAndModify: false
   })
   .then(() => {
     app.emit('pronto');
